1e553518e80125b9bc69ef12776573214d128429
Puchek32 <puchek11@yandex.ru>
2021-11-17 18:52:24 +0300

    :) have a nice day :)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
@@ -18,5 +24,29 @@ public class Util {
         }
         return connection;
     }
+    private static SessionFactory concreteSessionFactory;
 
+    static {
+        try {
+            Properties prop = new Properties();
+            prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydb");
+            prop.setProperty("hibernate.connection.username", "root");
+            prop.setProperty("hibernate.connection.password", "root");
+            prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+
+            prop.setProperty("hibernate.hbm2ddl.auto", "update");
+
+            concreteSessionFactory = new org.hibernate.cfg.Configuration()
+                    .addProperties(prop)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory()
+            ;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return concreteSessionFactory.openSession();
+    }
 }
